04c20d203baecc778dbb722f3bc0cd845b624b99
Rajesh-Khanna <chrajeshkhanna82798@gmail.com>
2018-01-20 13:32:23 +0530

    build.py complete

--- olympics-mini-project/build.py
+++ olympics-mini-project/build.py
@@ -1,30 +1,39 @@
 import pandas as pd
-
-
+from sklearn.cluster import MeanShift,KMeans
 def load_data():
-    """
-        Enter your code here
-    """
-
+    df = pd.read_csv('data/olympics.csv',skiprows=1)
+    old_names = df.filter(like='01 !').columns.tolist()+df.filter(like='02 !').columns.tolist()+df.filter(like='03 !').columns.tolist()
+    new_names = ['Gold','Gold','Gold','Silver','Silver','Silver','Bronze','Bronze','Bronze']
+    df = df.rename(columns=dict(zip(old_names,new_names))) 
+    df[df.columns[0]] = df[df.columns[0]].apply(lambda x: x[:x.find('(')])
+    df = df.set_index(df.columns[0]) 
+    df = df.drop(['Total'])
+    return df
 
 def first_country(df):
-    """
-        Enter your code here
-    """
-
+    return df.iloc[0]
 
 def gold_medal(df):
-    """
-        Enter your code here
-    """
-
+    return df.ix[:,11].idxmax()
 
 def biggest_difference_in_gold_medal(df):
-    """
-        Enter your code here
-    """
+    return (df.ix[:,1]-df.ix[:,6]).idxmax()
 
 def get_points(df):
-    """
-    Enter your code here
-    """
+    return df.assign(Points= df.ix[:,11]*3 + df.ix[:,12]*2 + df.ix[:,13])
+
+def kmeans(df):
+    #We can use multiple k values and see which one gives best clustering. but to keep it short i used meanshift
+    ms = MeanShift()
+    ms.fit(df)
+    k = len(ms.cluster_centers_)
+    km = KMeans(n_clusters = k)
+    km.fit(df)
+    return km.cluster_centers_
+
+df = load_data()
+c1 = first_country(df)
+gc = gold_medal(df)
+dif = biggest_difference_in_gold_medal(df)
+df = get_points(df)
+print(kmeans(df))
